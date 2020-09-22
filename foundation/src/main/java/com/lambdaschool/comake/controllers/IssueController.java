e9f1eec7ab7a6cc8733f11fcd15682d60d0cca67
kodebae <karmenuopx@outlook.com>
2020-09-22 19:08:35 -0400

    set up issue controller

---
+++ foundation/src/main/java/com/lambdaschool/comake/controllers/IssueController.java
@@ -0,0 +1,94 @@
+package com.lambdaschool.comake.controllers;
+
+//import com.lambdaschool.comake.models.Issue;
+//import com.lambdaschool.comake.services.IssueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/issues")
+public class IssueController {
+    @Autowired
+    IssueService IssueService;
+
+    // http://localhost:2019/issues/issues
+    @GetMapping(value = "/issues",
+            produces = {"application/json"})
+    public ResponseEntity<?> listAllIssues(HttpServletRequest request) {
+        List<Issues> myIssues = issueService.findAll();
+        return new ResponseEntity<>(myIssues,
+                HttpStatus.OK);
+    }
+
+    // http://localhost:2019/issuess/issue/{issueId}
+    @GetMapping(value = "/issue/{issueId}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getIssueById(HttpServletRequest request,
+                                          @PathVariable
+                                                  Long issueId) {
+        Issue s = issueService.findIssueById(issueId);
+        return new ResponseEntity<>(s,
+                HttpStatus.OK);
+    }
+
+    // POST http://localhost:2019/issues/issue
+    @PostMapping(value = "/issue", consumes = "application/json")
+    public ResponseEntity<?> addNewIssue(@Valid @RequestBody Issue newIssue) throws
+            URISyntaxException {
+        newIssue.setIssueid(0);
+        newIssue = issueService.save(newIssue);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newIssueURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{issueid}")
+                .buildAndExpand(newIssue.getIssueid())
+                .toUri();
+        responseHeaders.setLocation(newIssueURI);
+
+        return new ResponseEntity<>(null,
+                responseHeaders,
+                HttpStatus.CREATED);
+    }
+
+    // PUT http://localhost:2019/issues/issue/1
+    @PutMapping(value = "/issue/{issueid}",
+            consumes = "application/json")
+    public ResponseEntity<?> updateFullIssue(
+            @Valid
+            @RequestBody
+                    Issue updateIssue,
+            @PathVariable
+                    long issueid) {
+        updateIssue.setIssueid(issueid);
+        issueService.save(updateIssue);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // DELETE http://localhost:2019/issues/issue/1
+    @DeleteMapping(value = "/issue/{id}")
+    public ResponseEntity<?> deleteIssueById(
+            @PathVariable
+                    long id) {
+        issueService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ foundation/src/main/java/com/lambdaschool/comake/repository/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.comake.repository;
+
+import com.lambdaschool.comake.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+        extends CrudRepository<Useremail, Long>
+{
+}
